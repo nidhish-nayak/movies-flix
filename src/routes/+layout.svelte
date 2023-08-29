930d1f90bcf90c5346895d1f862de9582b3c1b27
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-08-29 23:39:50 +0530

    Added 2 more routes and navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,29 @@
 	import '../app.postcss';
 </script>
 
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/popular">Popular</a></li>
+		<li><a href="/tv-series">TV Series</a></li>
+	</ul>
+</nav>
 <slot />
+
+<style>
+	ul {
+		display: flex;
+		justify-content: center;
+	}
+
+	li {
+		margin: 1em;
+		padding: 0.5em;
+		background-color: #ececec;
+		border-radius: 2px;
+	}
+
+	li:hover {
+		background-color: #cecece;
+	}
+</style>
---
+++ src/routes/tv-series/+page.svelte
@@ -0,0 +1,1 @@
+<h1>This is TV SERIES Route</h1>
