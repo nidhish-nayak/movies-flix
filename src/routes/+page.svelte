70e7472fcdefac6bd31c39c3c82c7e61741bd68c
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-09 22:27:36 +0530

    Removed comment from +page.svelte route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
 
 <main>
 	<h1 class="p-2 mt-6 text-xl font-semibold text-center">New Release</h1>
-
 	{#if data && data.props && data.props.nowPlaying}
 		<ul class="flex flex-wrap justify-center p-4">
 			{#each data.props.nowPlaying as movie (movie.id)}
